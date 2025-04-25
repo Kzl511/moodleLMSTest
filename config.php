a2f71ff3de7e14674147cb9b70fc85031eec3f3a
Kzl511 <kyawlinn.23@ichat.sp.edu.sg>
2025-04-25 10:35:00 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'sqlsrv';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = '10.64.2.18';
+$CFG->dbname    = 'moodleLMS';
+$CFG->dbuser    = 'moodleLMSAdmin';
+$CFG->dbpass    = 'absolute6464!';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 1433,
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://10.64.23.83/moodle';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
