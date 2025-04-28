b8a973b4528c9b22c39950a95f466697d1e058ff
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-28 10:57:23 +0800

    4th commit

--- local/message/lib.php
+++ local/message/lib.php
@@ -13,3 +13,7 @@
     // For now, let's just add a simple message for testing:
     $hook->add_html('<div class="local-message-footer">Hello from local message plugin</div>');
 }
+
+function local_message_extend_hook() {
+    core\hook\output\before_footer_html_generation::register_callback('local_message_extend_before_footer_html_generation');
+}
