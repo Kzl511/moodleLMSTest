c73c6522b668a1042f2147f5d4775160384a5a87
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-28 09:37:25 +0800

    testing before footer hook

---
+++ local/message/lib.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    localmessage
+ * @author     KZL
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+ */
+
+function local_message_before_footer() {
+    die('Hello');
+}
