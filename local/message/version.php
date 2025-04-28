8ef4612ab132a8110a4b8b3668f0c1ea11ab8fa3
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-28 09:28:43 +0800

    set up for local message

---
+++ local/message/version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Version details
+ *
+ * @package    localmessage
+ * @author     KZL
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2025041400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2025040800;        // Requires this Moodle version.
+$plugin->component = 'local_message';      // Full name of the plugin (used for diagnostics)
