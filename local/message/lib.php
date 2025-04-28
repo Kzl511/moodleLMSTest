a45938af37b5662bbc5a442a3d89030e23a3854c
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-28 10:54:37 +0800

    test 3

--- local/message/lib.php
+++ local/message/lib.php
@@ -8,6 +8,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-function local_message_before_footer() {
-    die('Hello');
+ function local_message_extend_before_footer_html_generation($hook) {
+    // Your custom logic here (e.g., adding HTML or performing actions before footer).
+    // For now, let's just add a simple message for testing:
+    $hook->add_html('<div class="local-message-footer">Hello from local message plugin</div>');
 }
