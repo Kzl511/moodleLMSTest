24bae5d39ee7dec623daaa12c647bcd777ac6ad8
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-29 17:00:26 +0800

    error 3

--- blocks/quicknote/db/access.php
+++ blocks/quicknote/db/access.php
@@ -1,10 +1,27 @@
 <?php
 
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = [
+
+    'block/quicknote:addinstance' => [
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ],
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ],
+
     'block/quicknote:myaddinstance' => [
         'captype' => 'write',
         'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => ['user' => CAP_ALLOW],
-        'clonepermissionsfrom' => 'moodle/my:manageblocks',
-    ],
+        'archetypes' => [
+            'user' => CAP_ALLOW,
+        ],
+
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ]
 ];
