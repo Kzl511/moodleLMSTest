1e4326c1e6ac8c9cf94efede6c8217c6fc077fcc
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-30 16:44:53 +0800

    error 4

--- local/welcomebanner/db/hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-return [
-    'core\hook\output\before_footer_html_generation' => [
-        'local_welcomebanner\hook\output\before_footer_html_generation_hook',
-    ],
-];
