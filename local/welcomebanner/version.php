d23525c6696375fbd6a4fd497bd8e3fa1fe0bf67
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-30 16:31:30 +0800

    error 2

--- local/welcomebanner/version.php
+++ local/welcomebanner/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_welcome';
+$plugin->component = 'local_welcomebanner';
 $plugin->version = 2025041400;
 $plugin->requires = 2025040800;
