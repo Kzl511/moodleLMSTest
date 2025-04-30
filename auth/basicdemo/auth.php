43acca0735153249d446f0a785e9e3f6583caf57
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-30 10:00:10 +0800

    connect with database

--- auth/basicdemo/auth.php
+++ auth/basicdemo/auth.php
@@ -11,34 +11,45 @@ class auth_plugin_basicdemo extends auth_plugin_base {
     }
 
     public function user_login($username, $password) {
-        $valid_users = [
-            'demo' => 'password123',
-            'admin' => 'adminpass',
-        ];
+        // $valid_users = [
+        //     'demo' => 'password123',
+        //     'admin' => 'adminpass',
+        // ];
 
-        if (isset($valid_users[$username]) && $valid_users[$username] === $password) {
-            return true;
+        // if (isset($valid_users[$username]) && $valid_users[$username] === $password) {
+        //     return true;
+        // }
+        // return false;
+
+        global $DB;
+
+        $record = $DB->get_record('basicdemo_users', ['username' => $username]);
+
+        if ($record) {
+            if ($record->password === $password) {
+                return true;
+            }
         }
         return false;
     }
 
-    public function can_create_users() {
-        return true;
-    }
+    // public function can_create_users() {
+    //     return true;
+    // }
 
-    public function create_user($user, $password) {
-        global $DB;
+    // public function create_user($user, $password) {
+    //     global $DB;
 
-        $user->auth = $this->authtype;
-        $user->id = user_create_user($user, false, false);
-        return $user->id;
-    }
+    //     $user->auth = $this->authtype;
+    //     $user->id = user_create_user($user, false, false);
+    //     return $user->id;
+    // }
 
-    public function user_update_password($user, $newpassword) {
-        return true;
-    }
+    // public function user_update_password($user, $newpassword) {
+    //     return true;
+    // }
 
-    public function loginpage_hook() {
+    // public function loginpage_hook() {
         
-    }
+    // }
 }
