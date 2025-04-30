b2a6405aaa8d7b58dcf4305bc80eb4cfbf2ad9a8
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-04-30 09:48:28 +0800

    error 3

--- auth/basicdemo/lang/auth_basicdemo.php
+++
@@ -1,3 +0,0 @@
-<?php
-$string['pluginname'] = 'Basic Demo Auth';
-$string['auth_basicdemodescription'] = 'A simple demo authentication plugin that uses hardcoded credentials.';
---
+++ auth/basicdemo/lang/en/auth_basicdemo.php
@@ -0,0 +1,3 @@
+<?php
+$string['pluginname'] = 'Basic Demo Auth';
+$string['auth_basicdemodescription'] = 'A simple demo authentication plugin that uses hardcoded credentials.';
