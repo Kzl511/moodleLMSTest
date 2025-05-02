68a944ccacf82b8bb3e2b1f9940048f96b51287c
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-05-02 17:28:17 +0800

    Delete Singpass

--- auth/singpass/lang/en/auth_singpass.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string['pluginname'] = 'Singpass Login';
-$string['loginsingpass'] = 'Login with Singpass';
-$string['auth_singpass_noaccount'] = 'No account found for your Singpass ID. Please contact the administrator.';
--- auth/singpass/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'auth_singpass';
-$plugin->version = 2025041400;
-$plugin->requires = 2025040800;
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v1.0';
