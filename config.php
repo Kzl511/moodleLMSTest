38ffce4fa4725cf53857e00cad02c327807332d8
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-05-05 08:54:09 +0800

    retract

--- config.php
+++ config.php
@@ -21,7 +21,6 @@ $CFG->wwwroot   = 'https://8e5c-118-189-156-214.ngrok-free.app/moodle';
 $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 $CFG->sslproxy = true;
-$CFG->auth = 'manual';
 
 $CFG->directorypermissions = 0777;
 
