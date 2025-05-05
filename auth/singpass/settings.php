b2993266e49ee9d3f67b30b27aae71fc51daf440
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-05-05 16:26:43 +0800

    error 1

--- auth/singpass/settings.php
+++ auth/singpass/settings.php
@@ -4,13 +4,13 @@ defined('MOODLE_INTERNAL') || die();
 $settings->add(new admin_setting_configtext(
     'auth_singpass/clientid',
     'Singpass App ID',
-    'dfkZ2MwYtkmA01ZVY9B5UOze1ocNWKy5',
-    ''
+    'Your registered Singpass application ID.',
+    'dfkZ2MwYtkmA01ZVY9B5UOze1ocNWKy5'
 ));
 
 $settings->add(new admin_setting_configtext(
     'auth_singpass/redirecturi',
     'Redirect URI',
-    'https://8e5c-118-189-156-214.ngrok-free.app/moodle/auth/singpass/redirect.php',
-    ''
+    'Your registered Singpass redirect URI.',
+    'https://8e5c-118-189-156-214.ngrok-free.app/moodle/auth/singpass/redirect.php'
 ));
