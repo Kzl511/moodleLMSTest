5da8c92cfa836a10d3fdbdb0d3c8698ea837779f
Kyaw Zayar Linn <zayarlinn.kyaw@SG-AKC.com>
2025-05-05 09:17:37 +0800

    new singpass login page

---
+++ auth/singpass/singpass_login.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__ . '/../../config.php');
+require_once($CFG->libdir . '/outputrenderers.php');
+
+// Set up the page
+$PAGE->set_url(new moodle_url('/auth/singpass/singpass_login.php'));
+$PAGE->set_context(context_system::instance());
+$PAGE->set_title('Login with Singpass');
+$PAGE->set_heading('Login with Singpass');
+$PAGE->set_pagelayout('login');
+
+// Output starts
+echo $OUTPUT->header();
+echo $OUTPUT->heading("Welcome to Singpass Login");
+
+// Create the button (change redirect URL later as needed)
+$buttonurl = new moodle_url('/auth/singpass/redirect.php');
+$buttonhtml = html_writer::link($buttonurl, 'Login with Singpass', [
+    'class' => 'btn btn-primary',
+    'style' => 'margin-top: 20px; display: inline-block; padding: 10px 20px; font-size: 1.2em;',
+]);
+
+echo html_writer::div($buttonhtml, 'singpass-login-button', ['style' => 'text-align:center;']);
+
+echo $OUTPUT->footer();
